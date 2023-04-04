c55197ad6c722a7d805d05d91bde0c594700127a
Tanmay Patel <tanmay.1rivet@gmail.com>
2023-04-04 19:47:10 +0530

    mantine provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
+import { MantineProvider, Button, TextInput, Loader } from '@mantine/core';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MantineProvider withGlobalStyles withNormalizeCSS
+        theme={{
+          fontFamily: 'monospace',
+          // colorScheme: 'dark',
+          primaryColor: 'grape',
+          // defaultRadius: "lg",
+          radius: {
+            md: "1rem",
+          },
+          fontSizes: {
+            md: "0.6rem"
+          },
+          headings: {
+            fontFamily: 'Roboto, sans-serif',
+            sizes: {
+              h1: { fontSize: '20rem' }
+            }
+          },
+          // focusRingStyles: {
+          //   styles: (theme) => ({ outline: `${rem(2)} solid ${theme.colors.orange[5]}` })
+          // }
+          defaultGradient: {
+            from: 'blue',
+            to: 'red',
+            deg: 90,
+          },
+          loader: "dots"
+        }}>
+        <h1>Hello, Mantine UI!</h1>
+        <Button>Click me!</Button>
+        <h4>Hello, simple text!</h4>
+        <TextInput placeholder="Focus input to see styles override" />
+        <Button variant='gradient'>Gradient!</Button>
+        <Loader size="md" />
+      </MantineProvider>
     </div>
   );
 }
