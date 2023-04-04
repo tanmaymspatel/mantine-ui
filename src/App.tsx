600ab1a58f0feb5f579e323406b5b976bb303567
Tanmay Patel <tanmay.1rivet@gmail.com>
2023-04-04 20:17:26 +0530

    usemantinetheme hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
-import { MantineProvider, Button, TextInput, Loader } from '@mantine/core';
+import { MantineProvider, Button, TextInput, Loader, Group, Checkbox, Radio, useMantineTheme } from '@mantine/core';
 import './App.css';
 
 function App() {
+  const theme = useMantineTheme();
   return (
     <div className="App">
       <MantineProvider withGlobalStyles withNormalizeCSS
@@ -30,14 +31,24 @@ function App() {
             to: 'red',
             deg: 90,
           },
-          loader: "dots"
+          loader: "dots",
+          cursorType: "pointer",
         }}>
         <h1>Hello, Mantine UI!</h1>
         <Button>Click me!</Button>
         <h4>Hello, simple text!</h4>
-        <TextInput placeholder="Focus input to see styles override" />
+        <TextInput my="xl" placeholder="Focus input to see styles override" />
         <Button variant='gradient'>Gradient!</Button>
         <Loader size="md" />
+        <Group mt="xl">
+          <Checkbox label="Default checkbox" />
+          <Radio label="Default radio" value="pointer" />
+        </Group>
+        <p style={{ background: theme.colors.blue[9] }}>hello, useMantinetheme hook</p>
+        <MantineProvider theme={{ fontFamily: 'Greycliff CF, sans-serif', primaryColor: "cyan", loader: "bars" }}>
+          <Button my="xl">Greycliff CF button</Button>
+          <Loader size="md" color='grape' />
+        </MantineProvider>
       </MantineProvider>
     </div>
   );
